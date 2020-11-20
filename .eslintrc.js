07c72ae3302e5bac9159734c9d0bfe5c42142169
zhengjialux <15879398507@163.com>
2020-11-20 10:25:47 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'prettier', "plugin:prettier/recommended", 'plugin:react/recommended'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier', "plugin:prettier/recommended"],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
